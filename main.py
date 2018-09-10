ed68298d64873965dbf37976baaf50db7cf7c660
bornfreeonekiss3 <lidantong123@163.com>
2018-09-10 15:37:53 +0800

    JsonDecodeError

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ import json
 index = 0
 productid_list = []
 out_list = []
+ID = []
 
 phone_head_url = 'https://search.jd.com/Search?keyword=%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&cid2=653&cid3=655&page='
 phone_tail_url = '&s=217&click=0'
 
-phoneid_head_url = 'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv8165&productId='
+phoneid_head_url = 'https://sclub.jd.com/comment/productPageComments.action?&productId='
 phoneid_middile_url = '&score=0&sortType=5&page='
 phoneid_tail_url = '&pageSize=10&isShadowSku=0&fold=1'
 
 
 def getID(url):
-    for index in range(1, 100):
+    for index in range(1, 2):
         re = requests.get(url)
         soup = BeautifulSoup(re.text, 'lxml')
         a_list = soup.find_all('li', class_='gl-item')
@@ -48,12 +49,33 @@ def getComment(url):
                 out_list.append(new_item)
     return out_list
 
+def getallID() :
+    for index in range(1, 2):
+        phone_url = phone_head_url + str(index) + phone_tail_url
+        id = getID(phone_url)
+        ID.append(id)
+    return ID
 
-for index in range(1, 2):
-    phone_url = phone_head_url + str(index) + phone_tail_url
-    id = getID(phone_url)
+def flatten(input_list):
+    output_list = []
+    while True :
+        if input_list == []:
+            break
+        for index , i in enumerate(input_list):
+            if type(i) == list :
+                input_list = i + input_list[index+1:]
+                break
+            else :
+                output_list.append(i)
+                input_list.pop(index)
+                break
 
-for i in id:
+    return output_list
+            
+originID = getallID()
+finalID = flatten(originID)
+
+for i in finalID:
     phoneid_origin_url = phoneid_head_url + str(i) + phoneid_middile_url
     for index in range(1, 100):
         phoneid_url = phoneid_origin_url + str(index) + phoneid_tail_url
